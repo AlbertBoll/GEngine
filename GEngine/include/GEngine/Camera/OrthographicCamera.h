bce23f721293bbab444b18224a703deb1dfc6cf8
AlbertBoll <albertboll19900124@gmail.com>
2022-05-06 12:07:33 -0700

    Implement Orthographic Camera derived from CameraBase

--- GEngine/include/GEngine/Camera/OrthographicCamera.h
+++ GEngine/include/GEngine/Camera/OrthographicCamera.h
@@ -6,6 +6,16 @@ namespace GEngine::Camera
 	class OrthographicCamera : public CameraBase
 	{
 
+	public:
+		OrthographicCamera(float left = -1.f, float right = 1.f, float bottom = -1.f,
+			float top = 1.f, float near_ = -1.f, float far_ = 1.f);
+
+		void SetOrthographic(float left = -1.f, float right = 1.f, float bottom = -1.f,
+			float top = 1.f, float near_ = -1.f, float far_ = 1.f);
+
+		void OnResize(int new_width, int new_height) override;
+
+		void OnScroll(float new_zoom_level)override{}
 	};
 }
 
--- GEngine/src/Camera/OrthographicCamera.cpp
+++ GEngine/src/Camera/OrthographicCamera.cpp
@@ -1,2 +1,40 @@
 #include "gepch.h"
 #include "Camera/OrthographicCamera.h"
+#include "Math/Matrix.h"
+
+namespace GEngine::Camera
+{
+
+	OrthographicCamera::OrthographicCamera(float left, float right, float bottom, float top, float near_, float far_): CameraBase()
+	{
+		
+		SetOrthographic(left, right, bottom, top, near_, far_);
+	}
+
+	void OrthographicCamera::SetOrthographic(float left, float right, float bottom, float top, float Near, float Far)
+	{
+		m_CameraSetting.m_OrthographicSetting.m_Left = left;
+		m_CameraSetting.m_OrthographicSetting.m_Right = right;
+		m_CameraSetting.m_OrthographicSetting.m_Buttom = bottom;
+		m_CameraSetting.m_OrthographicSetting.m_Top = top;
+		m_CameraSetting.m_OrthographicSetting.m_Near = Near;
+		m_CameraSetting.m_OrthographicSetting.m_Far = Far;
+		
+		m_Projection = Matrix::MakeOrthographic(left, right, bottom, top, Near, Far);
+		RecalculateViewProjection();
+	
+	}
+
+	void OrthographicCamera::OnResize(int new_width, int new_height)
+	{
+
+
+		float aspectRatio = static_cast<float>(new_width) / static_cast<float>(new_height);
+		float near_ = m_CameraSetting.m_OrthographicSetting.m_Near;
+		float far_ = m_CameraSetting.m_OrthographicSetting.m_Far;
+		SetOrthographic(-aspectRatio, aspectRatio, -aspectRatio, aspectRatio, near_, far_);
+	
+	}
+
+
+}
