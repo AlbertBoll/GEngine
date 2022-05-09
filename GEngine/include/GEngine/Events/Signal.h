5a045aea3edbd058217e66a116b13869aff12d32
AlbertBoll <albertboll19900124@gmail.com>
2022-05-09 11:14:31 -0700

    Change different implementation for signal and slot to avoid virtual function and dynamical dispatch

--- GEngine/include/GEngine/Events/Signal.h
+++ GEngine/include/GEngine/Events/Signal.h
@@ -4,10 +4,391 @@
 
 namespace GEngine
 {
+#define ConnectTo(sender, signal, receiver, className ,slot) (sender).signal.Connect<className, slot>(receiver)
 
-	#define Connect(sender, signal, receiver, slot) (sender)->signal.RegisterSlot(receiver, slot)
+    template <typename Signature>
+    class Delegate;
 
-	template<typename>class SlotBase;
+    /**** namespace scope swap function ****/
+    template <typename Signature>
+    void swap(Delegate<Signature>& d1, Delegate<Signature>& d2)
+    {
+        d1.Swap(d2);
+    }
+
+    /**** delegate partial class template for function types ****/
+    template <typename Ret, typename... Args>
+    class Delegate<Ret(Args...)>
+    {
+       inline static int currentTotalID = 0;
+    public:
+        Delegate();
+
+        Delegate(Delegate const& other);
+
+        Delegate(Delegate&& other);
+
+        ~Delegate();
+
+        Delegate& operator=(Delegate const& other);
+
+        Delegate& operator=(Delegate&& other);
+
+        template <Ret(*FreeFunction)(Args...)>
+        void Bind();
+
+        template <typename Type, Ret(Type::* PtrToMemFun)(Args...)>
+        void Bind(Type& instance);
+
+        template <typename Type, Ret(Type::* PtrToConstMemFun)(Args...) const>
+        void Bind(Type& instance);
+
+        template <typename Type>
+        void Bind(Type& funObj);
+
+        template <typename Type>
+        void Bind(Type&& funObj);
+
+        void Swap(Delegate& other);
+
+        explicit operator bool() const { return mFunction != nullptr; }
+
+        Ret operator()(Args... args) { return mFunction(&mData, std::forward<Args>(args)...); }
+        Ret Invoke(Args... args) { return mFunction(&mData, std::forward<Args>(args)...); }
+
+        int GetID() const { return mID; }
+
+    private:
+        //typedef typename std::aligned_storage<sizeof(void*), alignof(void*)>::type Storage;
+        using Storage = std::aligned_storage_t<sizeof(void*), alignof(void*)>;
+        using Function = Ret(*)(void*, Args...);
+
+        Storage mData;
+        Function mFunction;
+
+        bool mStored = false;
+        int mID = 0;
+
+        using DestroyStorageFunction = void(*)(Delegate*);
+        using CopyStorageFunction = void(*)(const Delegate*, Delegate*);
+        using MoveStorageFunction = void(*)(Delegate*, Delegate*);
+
+        DestroyStorageFunction mDestroyStorage = nullptr;
+        CopyStorageFunction mCopyStorage = nullptr;
+        MoveStorageFunction mMoveStorage = nullptr;
+
+        /**** helper function templates for special member functions ****/
+        template <typename Type>
+        static void DestroyStorage(Delegate* delegate)
+        {
+            reinterpret_cast<Type*>(&delegate->mData)->~Type();
+        }
+
+        template <typename Type>
+        static void CopyStorage(const Delegate* src, Delegate* dst)
+        {
+            new(&dst->mData) Type(*reinterpret_cast<const Type*>(&src->mData));
+        }
+
+        template <typename Type>
+        static void MoveStorage(Delegate* src, Delegate* dst)
+        {
+            new(&dst->mData) Type(std::move(*reinterpret_cast<Type*>(&src->mData)));
+        }
+
+        /**** stub functions ****/
+        template <Ret(*FreeFunction)(Args...)>
+        static Ret Stub(void* data, Args... args)
+        {
+            return FreeFunction(std::forward<Args>(args)...);
+        }
+
+        template <typename Type, Ret(Type::* PtrToMemFun)(Args...)>
+        static Ret Stub(void* data, Args... args)
+        {
+            Storage* storage = static_cast<Storage*>(data);
+            Type* instance = *reinterpret_cast<Type**>(storage);
+
+            return (instance->*PtrToMemFun)(std::forward<Args>(args)...);
+        }
+
+        template <typename Type, Ret(Type::* PtrToConstMemFun)(Args...) const>
+        static Ret Stub(void* data, Args... args)
+        {
+            Storage* storage = static_cast<Storage*>(data);
+            Type* instance = *reinterpret_cast<Type**>(storage);
+
+            return (instance->*PtrToConstMemFun)(std::forward<Args>(args)...);
+        }
+
+        template <typename Type>
+        static Ret Stub(void* data, Args... args)
+        {
+            Storage* storage = static_cast<Storage*>(data);
+            Type* instance = *reinterpret_cast<Type**>(storage);
+
+            return (*instance)(std::forward<Args>(args)...);
+        }
+
+        template <typename Type, typename>
+        static Ret Stub(void* data, Args... args)
+        {
+            Storage* storage = static_cast<Storage*>(data);
+            Type* instance = reinterpret_cast<Type*>(storage);
+
+            return (*instance)(std::forward<Args>(args)...);
+        }
+    };
+
+    template <typename Ret, typename... Args>
+    Delegate<Ret(Args...)>::Delegate()
+    {
+
+        mID = currentTotalID++;
+
+        new(&mData) std::nullptr_t(nullptr);
+
+        mFunction = nullptr;
+    }
+
+    template <typename Ret, typename... Args>
+    Delegate<Ret(Args...)>::Delegate(Delegate const& other)
+    {
+        if (other.mStored)
+        {
+            other.mCopyStorage(&other, this);
+
+            mDestroyStorage = other.mDestroyStorage;
+            mCopyStorage = other.mCopyStorage;
+            mMoveStorage = other.mMoveStorage;
+           
+            mStored = true;
+        }
+        else
+        {
+            mData = other.mData;
+         
+        }
+
+        mID = other.mID;
+        mFunction = other.mFunction;
+    }
+
+    template <typename Ret, typename... Args>
+    Delegate<Ret(Args...)>::Delegate(Delegate&& other)
+    {
+        if (other.mStored)
+        {
+            other.mMoveStorage(&other, this);
+
+            mDestroyStorage = other.mDestroyStorage;
+            mCopyStorage = other.mCopyStorage;
+            mMoveStorage = other.mMoveStorage;
+            mStored = true;
+        }
+        else
+        {
+            mData = other.mData;
+        }
+
+        mFunction = other.mFunction;
+        mID = other.mID;
+    }
+
+    template <typename Ret, typename... Args>
+    Delegate<Ret(Args...)>::~Delegate()
+    {
+        if (mStored)
+            mDestroyStorage(this);
+    }
+
+    template <typename Ret, typename... Args>
+    Delegate<Ret(Args...)>& Delegate<Ret(Args...)>::operator=(Delegate const& other)
+    {
+        Delegate temp(other);
+        Swap(temp);
+
+        return *this;
+    }
+
+    template <typename Ret, typename... Args>
+    Delegate<Ret(Args...)>& Delegate<Ret(Args...)>::operator=(Delegate&& other)
+    {
+        Delegate temp(std::move(other));
+        Swap(temp);
+
+        return *this;
+    }
+
+    template <typename Ret, typename... Args>
+    template <Ret(*FreeFunction)(Args...)>
+    void Delegate<Ret(Args...)>::Bind()
+    {
+        new(&mData) std::nullptr_t(nullptr);
+
+        mFunction = &Stub<FreeFunction>;
+    }
+
+    template <typename Ret, typename... Args>
+    template <typename Type, Ret(Type::* PtrToMemFun)(Args...)>
+    void Delegate<Ret(Args...)>::Bind(Type& instance)
+    {
+        new(&mData) Type* (&instance);
+
+        mFunction = &Stub<Type, PtrToMemFun>;
+    }
+
+    template <typename Ret, typename... Args>
+    template <typename Type, Ret(Type::* PtrToConstMemFun)(Args...) const>
+    void Delegate<Ret(Args...)>::Bind(Type& instance)
+    {
+        new(&mData) Type* (&instance);
+
+        mFunction = &Stub<Type, PtrToConstMemFun>;
+    }
+
+    template <typename Ret, typename... Args>
+    template <typename Type>
+    void Delegate<Ret(Args...)>::Bind(Type& funObj)
+    {
+        new(&mData) Type* (&funObj);
+
+        mFunction = &Stub<Type>;
+    }
+
+    template <typename Ret, typename... Args>
+    template <typename Type>
+    void Delegate<Ret(Args...)>::Bind(Type&& funObj)
+    {
+        static_assert(sizeof(Type) <= sizeof(void*));
+
+        new(&mData) Type(std::move(funObj));
+
+        mFunction = &Stub<Type, Type>;
+
+        mDestroyStorage = &DestroyStorage<Type>;
+        mCopyStorage = &CopyStorage<Type>;
+        mMoveStorage = &MoveStorage<Type>;
+
+        mStored = true;
+    }
+
+    template <typename Ret, typename... Args>
+    void Delegate<Ret(Args...)>::Swap(Delegate& other)
+    {
+        if (other.mStored)
+            other.mCopyStorage(&other, this);
+        else
+            mData = other.mData;
+
+        Function tempFunction = mFunction;
+        mFunction = other.mFunction;
+        other.mFunction = tempFunction;
+
+        bool tempStored = mStored;
+        mStored = other.mStored;
+        other.mStored = tempStored;
+
+        DestroyStorageFunction tempDestroyFun = mDestroyStorage;
+        mDestroyStorage = other.mDestroyStorage;
+        other.mDestroyStorage = tempDestroyFun;
+
+        CopyStorageFunction tempCopyFun = mCopyStorage;
+        mCopyStorage = other.mCopyStorage;
+        other.mCopyStorage = tempCopyFun;
+
+        MoveStorageFunction tempMoveFun = mMoveStorage;
+        mMoveStorage = other.mMoveStorage;
+        other.mMoveStorage = tempMoveFun;
+    }
+
+
+    template <typename Signature>
+    class Signal;
+
+    /**** signal partial class template for function types ****/
+    template <typename Ret, typename... Args>
+    class Signal<Ret(Args...)>
+    {
+    public:
+        template <Ret(*FreeFunction)(Args...)>
+        auto& Connect();
+
+        template <typename Type, Ret(Type::* PtrToMemFun)(Args...)>
+        auto& Connect(Type& instance);
+
+        template <typename Type, Ret(Type::* PtrToConstMemFun)(Args...) const>
+        auto& Connect(Type& instance);
+
+        template <typename Type>
+        auto& Connect(Type&& funObj);
+
+   
+        void Disconnect(const Delegate<Ret(Args...)>& delegate)
+        {
+            mDelegates.erase(std::remove_if(mDelegates.begin(), mDelegates.end(), [&](const Delegate<Ret(Args...)>& d)
+
+                {
+
+                    return delegate.GetID() == d.GetID();
+
+                }), mDelegates.end());
+        }
+
+
+        explicit operator bool() const { return !mDelegates.empty(); }
+
+        void operator()(Args... args) { for (auto& delegate : mDelegates) delegate(std::forward<Args>(args)...); }
+        void Fire(Args... args) { for (auto& delegate : mDelegates) delegate.Invoke(std::forward<Args>(args)...); }
+
+    private:
+        std::vector<Delegate<Ret(Args...)>> mDelegates;
+    };
+
+    template <typename Ret, typename... Args>
+    template <Ret(*FreeFunction)(Args...)>
+    auto& Signal<Ret(Args...)>::Connect()
+    {
+        Delegate<Ret(Args...)> delegate;
+        delegate.template Bind<FreeFunction>();
+        mDelegates.push_back(delegate);
+        return mDelegates.back();
+        
+    }
+
+    template <typename Ret, typename... Args>
+    template <typename Type, Ret(Type::* PtrToMemFun)(Args...)>
+    auto& Signal<Ret(Args...)>::Connect(Type& instance)
+    {
+        Delegate<Ret(Args...)> delegate;
+        delegate.template Bind<Type, PtrToMemFun>(instance);
+        mDelegates.push_back(delegate);
+        return mDelegates.back();
+    }
+
+    template <typename Ret, typename... Args>
+    template <typename Type, Ret(Type::* PtrToConstMemFun)(Args...) const>
+    auto& Signal<Ret(Args...)>::Connect(Type& instance)
+    {
+        Delegate<Ret(Args...)> delegate;
+        delegate.template Bind<Type, PtrToConstMemFun>(instance);
+        mDelegates.push_back(delegate);
+        return mDelegates.back();
+    }
+
+    template <typename Ret, typename... Args>
+    template <typename Type>
+    auto& Signal<Ret(Args...)>::Connect(Type&& funObj)
+    {
+        Delegate<Ret(Args...)> delegate;
+        delegate.template Bind<Type>(std::forward<Type>(funObj));
+        mDelegates.push_back(delegate);
+        return mDelegates.back();
+    }
+
+
+# if 0
+	/*template<typename>class SlotBase;
 
 	template<typename R, typename ... Args>
 	class SlotBase<R(Args...)>
@@ -69,9 +450,9 @@ namespace GEngine
 	private:
 		std::vector<SlotBase<R(Args...)>*> m_CallBackList;
 
-	};
+	};*/
 
 
-
+#endif
 
 }
