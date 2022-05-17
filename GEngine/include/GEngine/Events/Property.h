acd7ca0c63b2d5386e96810bae9a5405f2ccd4d2
AlbertBoll <albertboll19900124@gmail.com>
2022-05-16 17:04:33 -0700

    Add Property

---
+++ GEngine/include/GEngine/Events/Property.h
@@ -0,0 +1,58 @@
+#pragma once
+
+
+#define Setter(member) void SetProperty(const decltype(member)& p){\
+std::cout << "Property changes to "<< p.m_Property<<std::endl;\
+									 member = p;\
+}\
+
+#define Getter(member) decltype(member)::PropertyType Get_##member()const{\
+							return member;\
+}\
+                    
+
+
+namespace GEngine
+{
+	template<typename T>
+	class Property
+	{
+		
+	public:
+	
+		using PropertyType = T;
+
+		T m_Property{};
+
+
+		Signal<void(const Property&)> m_PropertySignal;
+
+		Property(const T& property) : m_Property{ property } {}
+
+		Property() = default;
+
+		Property(const Property& other)
+		{
+			m_Property = other.m_Property;
+		}
+
+		Property& operator = (const Property& other)
+		{
+			if (m_Property != other.m_Property)
+			{
+				m_Property = other.m_Property;
+				m_PropertySignal(*this);
+			}
+
+			return *this;
+		}
+
+		operator T()const { return m_Property; }
+
+		friend std::ostream& operator<<(std::ostream& os, const Property& m){ os << m.m_Property; return os; }
+	};
+
+
+
+}
+ 
