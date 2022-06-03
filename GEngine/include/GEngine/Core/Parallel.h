cf4739f28f9bde14051891810be29473d812946e
AlbertBoll <albertboll19900124@gmail.com>
2022-06-02 18:03:17 -0700

    Add data level parallel code

--- GEngine/include/GEngine/Core/Parallel.h
+++ GEngine/include/GEngine/Core/Parallel.h
@@ -1,10 +1,22 @@
 #pragma once
 #include "tbb/tbb/parallel_for.h"
+#include "tbb/tbb/parallel_reduce.h"
 #include "tbb/tbb/blocked_range2d.h"
 #include "tbb/tbb/blocked_range3d.h"
 
+
+
 namespace GEngine
 {
+
+
+    template<typename T>
+    struct RangeParams
+    {
+        T Start;
+        T End;
+        size_t Grainsize;
+    };
 
     enum class ExecutionPolicy { Serial, Parallel };
 
@@ -44,6 +56,66 @@ namespace GEngine
         IndexType beginIndexZ, IndexType endIndexZ,
         const Func& function);
 
+
+    template <typename IndexType, typename Func, typename Partitioner = tbb::auto_partitioner>
+    void ParallelRangeFor(
+        const RangeParams<IndexType>& Range,
+        const Func& function, const Partitioner& partitioner)
+    {
+        tbb::parallel_for(tbb::blocked_range<IndexType>(Range.Start, Range.End, Range.Grainsize),
+            [&function](const tbb::blocked_range<IndexType>& range1d) {
+                function(range1d.begin(), range1d.end());
+            }, partitioner);
+
+    }
+
+
+    template <typename Res, typename IndexType, typename RealBodyFunc, typename ReductionFunc, typename Partitioner = tbb::auto_partitioner>
+    Res ParallelReduce(
+        const RangeParams<IndexType>& Range, Res initVal,
+        const RealBodyFunc& realBody, const ReductionFunc& reduction ,const Partitioner& partitioner)
+    {
+        return tbb::parallel_reduce(tbb::blocked_range<IndexType>(Range.Start, Range.End, Range.Grainsize), initVal,
+            [&realBody](const tbb::blocked_range<IndexType>& range1d, float local_res) {
+                return realBody(range1d.begin(), range1d.end(), local_res);
+            }, reduction, partitioner);
+
+    }
+
+
+    template <typename IndexType, typename Func, typename Partitioner = tbb::auto_partitioner>
+    void ParallelRangeFor(
+        const RangeParams<IndexType>& xRange,
+        const RangeParams<IndexType>& yRange,
+        const Func& function, const Partitioner& partitioner)
+    {
+        if (xRange.Start > xRange.End || yRange.Start > yRange.End) return;
+        tbb::parallel_for(tbb::blocked_range2d<IndexType>(xRange.Start, xRange.End, xRange.Grainsize, yRange.Start, yRange.End, yRange.Grainsize),
+            [&function](const tbb::blocked_range2d<IndexType>& range2d)
+            {
+                function(range2d.rows().begin(), range2d.rows().end(), range2d.cols().begin(), range2d.cols().end());
+            }, partitioner);
+
+    }
+
+    template <typename IndexType, typename Func, typename Partitioner = tbb::auto_partitioner>
+    void ParallelRangeFor(
+        const RangeParams<IndexType>& xRange,
+        const RangeParams<IndexType>& yRange,
+        const RangeParams<IndexType>& zRange,
+        const Func& function, const Partitioner& partitioner)
+    {
+        if (xRange.Start > xRange.End || yRange.Start > yRange.End || zRange.Start > zRange.End) return;
+        tbb::parallel_for(tbb::blocked_range3d<IndexType>(xRange.Start, xRange.End, xRange.Grainsize, yRange.Start, yRange.End, yRange.Grainsize, zRange.Start, zRange.End, zRange.Grainsize),
+            [&function](const tbb::blocked_range3d<IndexType>& range3d)
+            {
+                function(
+                    range3d.rows().begin(),  range3d.rows().end(),
+                    range3d.cols().begin(),  range3d.cols().end(),
+                    range3d.pages().begin(), range3d.pages().end());
+            }, partitioner);
+
+    }
   
 
     template<typename IndexType, typename Func>
@@ -95,15 +167,16 @@ namespace GEngine
 
     }
 
-
+  
     template<typename IndexType, typename Func>
     void ParallelRangeFor(IndexType beginIndexX, IndexType endIndexX, IndexType beginIndexY, IndexType endIndexY, const Func& function)
     {
+
         if (beginIndexX > endIndexX || beginIndexY > endIndexY) return;
-        tbb::parallel_for(tbb::blocked_range2d<IndexType>(beginIndexX, endIndexX, beginIndexY, endIndexY),
+        tbb::parallel_for(tbb::blocked_range2d<IndexType>(beginIndexX, endIndexX, 64, beginIndexY, endIndexY, 64),
             [&function](const tbb::blocked_range2d<IndexType>& range2d)
             {
-                function(range2d.cols().begin(), range2d.cols().end(), range2d.rows().begin(), range2d.rows().end());
+                function(range2d.rows().begin(), range2d.rows().end(), range2d.cols().begin(), range2d.cols().end());
             });
 
     }
@@ -117,8 +190,9 @@ namespace GEngine
         tbb::parallel_for(tbb::blocked_range3d<IndexType>(beginIndexX, endIndexX, beginIndexY, endIndexY, beginIndexZ, endIndexZ),
             [&function](const tbb::blocked_range3d<IndexType>& range3d)
             {
-                function(range3d.cols().begin(),  range3d.cols().end(),
+                function(
                          range3d.rows().begin(),  range3d.rows().end(),
+                         range3d.cols().begin(),  range3d.cols().end(),
                          range3d.pages().begin(), range3d.pages().end());
             });
     }
