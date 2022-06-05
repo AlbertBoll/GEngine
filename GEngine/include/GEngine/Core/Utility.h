649d70d6b8894448a185f1071115156647aec72c
AlbertBoll <albertboll19900124@gmail.com>
2022-06-05 13:17:06 -0700

    Add some useful helper function

--- GEngine/include/GEngine/Core/Utility.h
+++ GEngine/include/GEngine/Core/Utility.h
@@ -91,5 +91,61 @@ namespace GEngine
 		else return std::false_type{};
 	}
 
+
+	//*****************************define universal hash function************************"""
+	template<typename T>
+	static inline void hash_combine(size_t& seed, const T& val)
+	{
+		seed ^= std::hash<T>()(val) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+	}
+
+
+	template<typename T>
+	static inline void hash_val(size_t& seed, const T& val)
+	{
+		hash_combine(seed, val);
+	}
+
+	template<typename T, typename... Types>
+	static inline void hash_val(size_t& seed, const T& val, const Types&... args)
+	{
+		hash_combine(seed, val);
+		hash_val(seed, args...);
+	}
+
+	template<typename... Types>
+	static inline size_t hash_val(const Types&... args)
+	{
+		size_t seed = 0;
+		hash_val(seed, args...);
+		return seed;
+	}
+
+	template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+	// explicit deduction guide (not needed as of C++20)
+	template<class... Ts> overloaded(Ts...)->overloaded<Ts...>;
+
+
+	struct Lambda {
+		template<typename Tret, typename T>
+		static Tret lambda_ptr_exec(void* data) {
+			return (Tret)(*(T*)fn<T>())(data);
+		}
+
+		template<typename Tret = void, typename Tfp = Tret(*)(void*), typename T>
+		static Tfp ptr(T& t) {
+			fn<T>(&t);
+			return (Tfp)lambda_ptr_exec<Tret, T>;
+		}
+
+		template<typename T>
+		static void* fn(void* new_fn = nullptr) {
+			static void* fn;
+			if (new_fn != nullptr)
+				fn = new_fn;
+			return fn;
+		}
+	};
+
 }
 
